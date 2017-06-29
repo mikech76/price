9eebdd9ae7647cc6ba6b89268b404e9ff9b4cd3b
Mikech <mikech76@gmail.com>
2017-06-30 00:55:11 +0700

    migrate - prices - products

---
+++ database/migrations/2017_06_29_172937_create_product_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function(Blueprint $table) {
+            $table->increments('id');
+
+            $table->integer('price_id')->unsigned();
+            $table->foreign('price_id')->references('id')->on('prices')->onDelete('cascade');
+
+            $table->string('name')->index();
+            $table->string('units');
+            $table->float('price');
+            $table->integer('quantity');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
